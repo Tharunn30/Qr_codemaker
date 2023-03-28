62b626a0aca08d2bd19560aab47375b2d3aa595c
P.V.Tharunn Raj <68377504+Tharunn30@users.noreply.github.com>
2023-03-28 20:49:54 +0530

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,7 +1,19 @@
 import qrcode
+from PIL import Image
 
-link = input("Enter the link you want to convert into qrcode: ")
-img = qrcode.make(link)
-print("Please save in png form")
-title = input("Enter the title you want to the qr code to be saved as: ")
-img.save(f"{title}.png")
+link = input("Enter the link you want to convert into QR code: ")
+
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10, border=4)
+qr.add_data(link)
+qr.make(fit=True)
+
+fill_color = input("Enter the fill color for the QR code (in HEX format): ")
+back_color = input("Enter the background color for the QR code (in HEX format): ")
+img = qr.make_image(fill_color=fill_color, back_color=back_color)
+
+title = input("Enter the filename to save the QR code as (without extension): ")
+filename = f"{title}.png"
+
+img.save(filename)
+print(f"QR code saved as {filename}")
